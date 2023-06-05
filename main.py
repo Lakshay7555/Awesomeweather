babef67552251609e6a070147ec19acf0675f124
Lakshay7555 <115101107+Lakshay7555@users.noreply.github.com>
2023-06-05 15:06:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ API_KEY = os.environ["API_KEY"]
 BASE_URL= "https://api.openweathermap.org/data/2.5/"
 
 
-app = Flask(__name__, static_url_path='', static_folder='web/static', template_folder='web/templates')
+app = Flask(__name__)
 
 
 @app.route('/')
