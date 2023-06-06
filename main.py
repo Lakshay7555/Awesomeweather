8d2df8ab1ea7ff148190c8df129221f8f0616b32
Lakshay7555 <115101107+Lakshay7555@users.noreply.github.com>
2023-06-06 07:01:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-import requests
-import os
 from flask import Flask
-
-API_KEY = os.environ["API_KEY"]
-BASE_URL= "https://api.openweathermap.org/data/2.5/"
-
-
 app = Flask(__name__)
 
 
-@app.route('/')
-def homepage():
-    return "hello"
+@app.route("/")
+def hello():
+    return "Hello, Mr. Weather!"
 
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
